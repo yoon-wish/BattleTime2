e7cb1203907c419bd8fbed1bc5b92d69f2eba2ad
yoon-wish <sowon5685@gmail.com>
2024-04-05 15:08:22 +0900

    [ADD] setStageVillage

--- BattleTime2/src/battleTime2/stage/StageVillage.java
+++ BattleTime2/src/battleTime2/stage/StageVillage.java
@@ -1,10 +1,36 @@
 package battleTime2.stage;
 
+import battleTime2.manager.GameManager;
+
 public class StageVillage extends Stage{
 
+	private final int STORE = 1;
+	private final int HOUSE = 2;
+	private final int LOBBY = 3;
+	
 	@Override
 	public boolean update() {
-		// TODO Auto-generated method stub
+		System.out.println("┌──────────────┐");
+		System.out.println("    ❶ 상점");
+		System.out.println("    ❷ 아지트");
+		System.out.println("    ❸ 로비");
+		System.out.println("└──────────────┘");
+
+		System.out.print("👉 ");
+		int sel = GameManager.sc.nextInt();
+
+		while (!((sel == STORE) || (sel == LOBBY) || (sel == HOUSE))) {
+			System.out.print("👉 ");
+			sel = GameManager.sc.nextInt();
+		}
+
+		if (sel == STORE)
+			GameManager.nextStage = "STORE";
+		else if(sel == HOUSE)
+			GameManager.nextStage = "HOUSE";
+		else if (sel == LOBBY)
+			GameManager.nextStage = "LOBBY";
+
 		return false;
 	}
 
